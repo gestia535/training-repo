a60dc9ea511edf8fe0fe560a75b1f19f3de52248
Alexandra Pavliukova <mashana555@gmail.com>
2024-06-26 16:45:24 +0700

    Добавлена функция сложения

---
+++ feature.py
@@ -0,0 +1,13 @@
+from random import random
+
+
+def calculate(a, b):
+    sum_numbers = a+b
+    return sum_numbers
+
+
+print(calculate(84793758457, 398758937459475))
+
+
+def choose_dog():
+    return random
